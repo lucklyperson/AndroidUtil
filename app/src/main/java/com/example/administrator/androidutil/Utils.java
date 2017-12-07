1cff9050887b0751bda3430295361a53efa6a329
wzq <18335447965@163.com>
2017-12-07 09:15:11 +0800

    新增log日志分段打印的工具

--- app/src/main/java/com/example/administrator/androidutil/Utils.java
+++ app/src/main/java/com/example/administrator/androidutil/Utils.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.WindowManager;
 
 import java.io.File;
@@ -222,6 +223,31 @@ public class Utils {
 
         }
 
+    }
+
+    /**
+     * log日志分段打印（针对log日志过长的情况）
+     *
+     * @param tag TAG
+     * @param msg msg
+     */
+    public static void log(String tag, String msg) {
+        if (tag == null || tag.length() == 0
+                || msg == null || msg.length() == 0)
+            return;
+
+        int segmentSize = 3 * 1024;
+        long length = msg.length();
+        if (length <= segmentSize) {// 长度小于等于限制直接打印
+            Log.e(tag, msg);
+        } else {
+            while (msg.length() > segmentSize) {// 循环分段打印日志
+                String logContent = msg.substring(0, segmentSize);
+                msg = msg.replace(logContent, "");
+                Log.e(tag, logContent);
+            }
+            Log.e(tag, msg);// 打印剩余日志
+        }
     }
 
 }
